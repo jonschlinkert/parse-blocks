66521353ce9a6195f36018b16777acc1e0510137
jonschlinkert <dev@sellside.com>
2014-11-23 06:30:57 -0500

    allow custom delimiters, ensure that locals are passed when using `get`

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * sections <https://github.com/jonschlinkert/sections>
+ * parse-blocks <https://github.com/jonschlinkert/parse-blocks>
  *
  * Copyright (c) 2014 Jon Schlinkert, contributors.
  * Licensed under the MIT license.
@@ -7,14 +7,11 @@
 
 'use strict';
 
-var fs = require('fs');
-var _ = require('lodash');
-var get = require('get-value');
+var delims = require('delimiter-regex');
+var extend = require('extend-shallow');
 
-var a = /\{\{#([^\\}]*(?:\\.[^\\}]*)*)\}\}/g;
-var b = /\{\{\/([^\\}]*(?:\\.[^\\}]*)*)\}\}/g;
-
-var parser = module.exports = function parser(str, locals) {
+module.exports = function(str, options) {
+  var opts = extend({open: ['{{#', '}}'], close: ['{{/', '}}']}, options);
   str = str.replace(/\r/g, '');
 
   var lines = str.split(/\n/);
@@ -22,85 +19,57 @@ var parser = module.exports = function parser(str, locals) {
   var i = 0;
 
   var name = 'definedoc';
+  var a = delims(opts.open);
+  var b = delims(opts.close);
+
   var stats = {};
   stats[name] = {};
 
   var isBlock = false;
   var cache = {};
   var match;
-  var n;
 
   while (i < len) {
     var line = lines[i++];
-    var o;
+    var o, n;
 
     if (match = a.exec(line)) {
       isBlock = true;
       o = {};
-
       var props = match[1].split(' ').filter(Boolean);
-
       // remove the helper name from the props array
       props.shift();
       // get the template key
       n = JSON.parse(props[0]);
+      cache[n] = {};
+
       // remove the template key from the props
       props.shift();
 
-      if (/\./.test(props[0])) {
-        props = get(locals, props[0]);
+      // If `locals` is passed, resolve object paths
+      if (opts.locals && /\./.test(props[0])) {
+        var get = require('get-value');
+        props = get(opts.locals, props[0]);
       }
 
-      o = {};
-      o.locals = props;
-      o.content = [];
-      o.orig = [];
+      cache[n].args = props;
+      cache[n].content = [];
+      cache[n].orig = [];
     }
 
     if (Boolean(line) && isBlock) {
-      o.content.push(line);
-      o.orig.push(line);
+      cache[n].content.push(line);
+      cache[n].orig.push(line);
     }
 
     if (b.exec(line)) {
-      o.content.shift();
-      o.content.pop();
-      o.content = o.content.join('\n');
+      cache[n].content.shift();
+      cache[n].content.pop();
+      cache[n].content = cache[n].content.join('\n');
       isBlock = false;
     }
 
-    cache[n] = o;
+    stats[name] = cache;
   }
-
-  stats[name] = cache;
   return stats;
 };
-
-var ctx = {
-  a: {b: {msg: 'one'}},
-  b: {c: {msg: 'two'}},
-  c: {d: {msg: 'three'}},
-  d: {e: {msg: 'four'}}
-};
-
-var matter = require('gray-matter');
-var aaa = parser(fs.readFileSync('fixtures/a.md', 'utf8'), ctx);
-
-var template = _.reduce(aaa, function (res, v, k) {
-  res[k] = _.reduce(v, function (acc, value, key) {
-    if (value && value.content) {
-      var res = matter(value.content);
-      acc[key] = _.omit(_.extend({}, value, res), 'lang');
-    }
-    return acc;
-  }, {});
-  return res;
-}, {});
-
-// var ccc = parser(fs.readFileSync('fixtures/c.md', 'utf8'));
-// var ddd = parser(fs.readFileSync('fixtures/d.md', 'utf8'));
-
-console.log(template);
-// console.log(aaa.definedoc.bar);
-// console.log(ccc);
-// console.log(ddd);
