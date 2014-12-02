aa479838a76ec5172829fef1a881195bacd42376
jonschlinkert <dev@sellside.com>
2014-12-02 18:58:43 -0500

    update tests and fixtures

--- test.js
+++ test.js
@@ -25,29 +25,64 @@ describe('should parse blocks', function () {
   it('should use the second argument in the opening block as the block key.', function () {
     var str = read('fixtures/a.txt');
     var res = blocks(str);
-    res.definedoc.should.have.properties('foo', 'bar', 'baz');
+    res.definedoc.should.have.properties('aaa', 'bbb', 'ccc');
   });
 
   it('should add any additional arguments to the `args` property.', function () {
     var str = read('fixtures/a.txt');
     var res = blocks(str);
-    res.definedoc.foo.should.have.property('args');
-    res.definedoc.foo.args.should.be.an.array;
+    res.definedoc.aaa.should.have.property('args');
+    res.definedoc.aaa.args.should.be.an.array;
   });
 
   it('should add inner content to the `content` property.', function () {
     var str = read('fixtures/a.txt');
     var res = blocks(str);
-    res.definedoc.foo.should.have.property('content');
+    res.definedoc.aaa.should.have.property('content');
   });
 
   it('should support custom delimiters.', function () {
     var str = read('fixtures/b.txt');
     var res = blocks(str, {open: ['<%-', '%>'], close: ['<%/', '%>']});
     res.should.have.property('definedoc');
-    res.definedoc.should.have.properties('foo', 'bar');
+    res.definedoc.should.have.properties('aaa', 'bbb');
+  });
+
+  it('should support custom block names.', function () {
+    var str = read('fixtures/d.txt');
+    var res = blocks(str, {name: 'apidoc'});
+    res.should.have.property('apidoc');
+    res.apidoc.should.have.properties('aaa', 'bbb');
+  });
+
+  it('should use `thisArg` as the context for any arguments passed to the block.', function () {
+    var ctx = {a: {name: 'AAA'}, b: {name: 'BBB'}, c: {name: 'CCC'}};
+    var str = read('fixtures/d.txt');
+    var res = blocks(str, {name: 'apidoc'}, ctx);
+
+    res.should.have.property('apidoc');
+    res.apidoc.should.have.properties('aaa', 'bbb', 'ccc');
+    res.apidoc.aaa.context.should.eql({name: 'AAA'});
+    res.apidoc.bbb.context.should.eql({name: 'BBB'});
+    res.apidoc.ccc.context.should.eql({name: 'CCC'});
+  });
+
+  it('should use `options.locals` as the context for any arguments passed to the block.', function () {
+    var ctx = {a: {name: 'AAA'}, b: {name: 'BBB'}, c: {name: 'CCC'}};
+    var str = read('fixtures/d.txt');
+    var res = blocks(str, {name: 'apidoc', locals: ctx});
+
+    res.should.have.property('apidoc');
+    res.apidoc.should.have.properties('aaa', 'bbb', 'ccc');
+    res.apidoc.aaa.context.should.eql({name: 'AAA'});
+    res.apidoc.bbb.context.should.eql({name: 'BBB'});
+    res.apidoc.ccc.context.should.eql({name: 'CCC'});
+  });
+
+  it('should support custom block names and custom delimiter syntax.', function () {
+    var str = read('fixtures/e.txt');
+    var res = blocks(str, {name: 'apidoc', open: ['<%-', '%>'], close: ['<%/', '%>']});
+    res.should.have.property('apidoc');
+    res.apidoc.should.have.properties('aaa', 'bbb', 'ccc');
   });
 });
-
-// var inspect = require('util').inspect;
-// console.log(inspect(blocks(read('fixtures/c.txt')), null, 10))
