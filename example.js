89ef330681ea7cd5b6d6e2ac51181394d6f92c63
jonschlinkert <dev@sellside.com>
2014-12-02 18:58:34 -0500

    adds example

---
+++ example.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var _ = require('lodash');
+var fs = require('fs');
+var inspect = require('util').inspect;
+var blocks = require('./');
+
+
+function read(str) {
+  return fs.readFileSync(str, 'utf8')
+}
+
+var str = read('fixtures/d.txt');
+var ctx = {a: {name: 'AAA'}, b: {name: 'BBB'}, c: {name: 'CCC'}};
+var res = blocks(str, {name: 'apidoc'}, ctx);
+
+_.forIn(res.apidoc, function (value, key) {
+  value.content = _.template(value.content, value.context);
+});
+
+console.log(res)
+
+
+// console.log(inspect(blocks(read('fixtures/d.txt')), null, 10));
+// console.log(inspect(blocks(read('fixtures/e.txt'), {name: 'apidoc', open: ['<%-', '%>'], close: ['<%/', '%>']}), null, 10));
+
+
